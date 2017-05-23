7a6019d3ddbd28e054d07516b826a57d80ed10fb
William Dana <william.b.dana@gmail.com>
2017-05-23 00:27:24 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |sea, array|
+    array.each do |info_hash|
+      if sea == season && info_hash["status"] == "Winner"
+        return info_hash["name"].slice(0..(info_hash["name"].index(' '))).strip
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |sea, array|
+    array.each do |info_hash|
+      if occupation == info_hash["occupation"]
+        return info_hash["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |sea, array|
+    array.each do |info_hash|
+      if hometown == info_hash["hometown"]
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |sea, array|
+    array.each do |info_hash|
+      if hometown == info_hash["hometown"]
+        return info_hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  arr = []
+  data.each do |sea, array|
+    array.each do |info_hash|
+      if sea == season
+        arr << info_hash["age"]
+      end
+    end
+  end
+  arr_i = arr.map! {|str| str.to_i}
+  total = arr_i.inject(:+)
+  length = arr_i.length
+  average = total.to_f / length.to_f
+  return average.round
 end
